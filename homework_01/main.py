b93ea55cdcf0c4bfcf816f63e6bba5580663fdc7
yelkin.md <yelkin.md@gmail.com>
2022-04-09 23:26:28 +0300

    Homework solved

---
+++ homework_01/main.py
@@ -0,0 +1,55 @@
+"""
+Домашнее задание №1
+Функции и структуры данных
+"""
+
+
+def power_numbers(*numbers):
+    return [number ** 2 for number in numbers]
+
+
+"""
+функция, которая принимает N целых чисел,
+и возвращает список квадратов этих чисел
+>>> power_numbers(1, 2, 5, 7)
+<<< [1, 4, 25, 49]
+"""
+
+
+# filter types
+ODD = "odd"
+EVEN = "even"
+PRIME = "prime"
+
+
+def is_prime(number):
+    not_prime = []
+    if number > 1:
+        if number <= 3:
+            return number
+        else:
+            for i in range(2, int(number**0.5+1)):
+                if number % i == 0:
+                    not_prime.append(number)
+                    break
+            if number not in not_prime: return number
+
+
+def filter_numbers(numbers_list, filter_type):
+    if filter_type == ODD:
+        return [number for number in numbers_list if number % 2 != 0]
+    if filter_type == EVEN:
+        return [number for number in numbers_list if number % 2 == 0]
+    if filter_type == PRIME:
+        return list(filter(is_prime, numbers_list))
+
+    """
+    функция, которая на вход принимает список из целых чисел,
+    и возвращает только чётные/нечётные/простые числа
+    (выбор производится передачей дополнительного аргумента)
+
+    >>> filter_numbers([1, 2, 3], ODD)
+    <<< [1, 3]
+    >>> filter_numbers([2, 3, 4, 5], EVEN)
+    <<< [2, 4]
+    """
